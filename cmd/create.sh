7c9b6e058021226af9a36c63d5c42d0d7341fc6c
Dashamir Hoxha <dashohoxha@gmail.com>
2017-08-29 11:41:07 +0200

    Fix cmd_create

--- cmd/create.sh
+++ cmd/create.sh
@@ -8,10 +8,10 @@ _EOF
 
 rename_function cmd_create orig_cmd_create
 cmd_create() {
+    mkdir -p plugins custom-css
+    touch custom-css/custom.css
+
     orig_cmd_create \
         --mount type=bind,src=$(pwd)/plugins,dst=/etc/schooltool/standard/plugins \
         --mount type=bind,src=$(pwd)/custom-css,dst=/etc/schooltool/standard/custom-css
-
-    mkdir -p plugins custom-css
-    touch custom-css/custom.css
 }
